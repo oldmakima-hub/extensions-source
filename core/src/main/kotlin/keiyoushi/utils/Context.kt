c30263ff8c26e6e8f2bf9a77d07764a7619a237d
oldmakima-hub <oldmakima@gmail.com>
2026-06-25 06:39:27 +0000

    Update Doujindesu scraper

--- core/src/main/kotlin/keiyoushi/utils/Context.kt
+++
@@ -1,7 +0,0 @@
-package keiyoushi.utils
-
-import android.app.Application
-import uy.kohesive.injekt.Injekt
-import uy.kohesive.injekt.api.get
-
-val applicationContext: Application get() = Injekt.get()
--- lib/e4p/build.gradle.kts
+++
@@ -1,9 +0,0 @@
-plugins {
-    alias(kei.plugins.library)
-}
-
-dependencies {
-    implementation("org.kotlincrypto.hash:blake2:0.8.0") {
-        exclude(group = "org.jetbrains.kotlin", module = "kotlin-stdlib")
-    }
-}
